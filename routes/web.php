c13a98d4b9ecc9787524dad31764fb43d3bdbf06
ilhamabi <ilhamabirizal365@gmail.com>
2026-06-29 23:09:34 +0700

    frontend: create dashboard dosen view, route, and css

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboard-dosen', function () {
+    return view('dosen.dashboard');
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
