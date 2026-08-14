d8a15dec347664bb8ca010fc8e3b152a33cf1df2
muhanaawahyu <muhanaawahyu00@gmail.com>
2026-08-14 14:27:56 +0700

    fix: unignore and add resources/views/letter/preview.blade.php

---
+++ resources/views/letter/preview.blade.php
@@ -0,0 +1,8 @@
+<x-letter.layout 
+    :title="$documentTitle ?? 'Preview Surat Resmi AMIKOM'" 
+    :signers="$signers ?? null" 
+    :signatureProps="$signatureProps ?? null" 
+    :qrToken="$qrToken ?? 'DEMO-TOKEN-2026'"
+    :isPlaceholder="$isPlaceholderMode ?? false">
+    {!! $bodyContent !!}
+</x-letter.layout>
